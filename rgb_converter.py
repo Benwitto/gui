a5437d5dec21b895f6c6ad22e43e62089f723b01
Benwitto <92056719+Benwitto@users.noreply.github.com>
2025-11-03 13:54:38 +0100

    Refactor rgb_converter to use URL and enhance UI
    Updated the RGB converter to fetch data from a URL instead of a local CSV file. Improved the user interface and output format for better usability.

--- rgb_converter.py
+++ rgb_converter.py
@@ -2,23 +2,49 @@ import streamlit as st
 import pandas as pd
 
 # get dy Data (HS Feed)
-#dy_feed = pd.read_csv(r"\\mftfile\Datenaustausch\onsite_marketing\tools\rgb_converter\hs_basefile.csv", sep=";", engine="python")
-#dy_feed.to_csv(r"\\mftfile\Datenaustausch\onsite_marketing\tools\rgb_converter\hs_basefile.csv", sep=";", index=False)
 url = "https://cdn.ullapopken.de/data/rgb_val/hs_basefile.csv"
 dy_feed = pd.read_csv(url, sep=";", engine="python")
 
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center; align-items: center;">
+        <img src="https://images.ullapopken.de/cms/alle/logos/happysize/brands/_y/brand_ulla_popken_500y_white.svg" 
+             width="200">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+st.write("")
+st.write("")
+st.write("")
 st.title("Artikelnummer to RGB-Identifier")
-st.text("Dieser Converter gibt dir zu einer Artikelnummer den richtigen RGB-Identifier.\nNutze den RGB-Identifier um dynamische Artikelpushes auf PLPs zu erstellen.")
+st.text("Dieser Converter gibt dir zu einen Artikelnummern den Kategorie Link mit den entsprechenden RGB-Identifiern.\nNutze die erstellte URL um dynamische Artikelpushes auf PLPs zu erstellen.")
+st.write("")
+# Eingabefeld (mehrzeilig)
+eingabe_url = st.text_input("PLP URL:")
 
 # Eingabefeld (mehrzeilig)
-eingabe = st.text_area("Artikelnummern:")
+eingabe_pn = st.text_area("Artikelnummern:")
 
 # Wenn Button gedrückt wird:
-if st.button("Covert ProductNumbers to RGB_IDs"):
+if st.button("Covert"):
+    st.write("")
+    st.write("")
     # Hier kommt deine Logik hin:
-    vals = eingabe.split("\n")
+    vals = eingabe_pn.split("\n")
     subset = dy_feed[dy_feed['ProductNumber'].isin(vals)]
     group_ids = subset['group_id'].tolist()
-    output = "\n".join(group_ids)
-    ergebnis = f"{output}"
-    st.text_area("Ausgabe:", value=ergebnis, height=200)
+    output = "&pushedArticleIds=" + "&pushedArticleIds=".join(group_ids)
+    ergebnis = f"{eingabe_url}{output}"
+    st.text_area("Ausgabe:", value=ergebnis, height=150)
+
+    st.write("")
+    st.text("Vorschau der Artikel:")
+    image_urls = subset['image_url'].tolist()
+    cols = st.columns(len(image_urls))
+    for col, url in zip(cols, image_urls):
+        with col:
+            st.markdown("<div style='padding: 0 10px;'>", unsafe_allow_html=True)
+            st.image(url, use_container_width=True)
+            st.markdown("</div>", unsafe_allow_html=True)
+
